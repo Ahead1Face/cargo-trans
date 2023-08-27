44d9ff9bf309517abe7bc55888b360ed8d876be5
Ahead1Face <ssatonin@yandex.ru>
2023-08-27 16:42:06 +0700

    krim marshrut

--- src/components/ImageWithText.jsx
+++ src/components/ImageWithText.jsx
@@ -4,7 +4,7 @@ function ImageWithText() {
             <div className='overlay-text'>
                 <div className='h1' style={{padding: '20px 0 '}}>Автомобильные грузоперевозки</div>
                 <div className='h2'>Доставка сборных и попутных грузов от 3 до 20 тонн</div>
-                <div className='h2'>По маршруту Москва-Нижний Новгород-Москва</div>
+                <div className='h2'>По маршруту Крым - Ростов-на-Дону - Крым</div>
                 <div className='h2'>От двери до двери</div>
             </div>
         </div>
